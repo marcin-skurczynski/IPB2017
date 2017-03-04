1d5328a67f27942935ef1decd3406f4d078c0ee3
pavltolmachev <pavltolmachev@yandex.ru>
2017-03-04 16:38:39 +0100

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -2,7 +2,14 @@ cardPin = 3471
 balance = 143.43
 
 pin = int(input("Enter your PIN: "))
+counter = 1
+
 while pin != cardPin:
+	if (counter >= 3):
+		print("Wrong PIN. You have run out of attempts. ")
+		exit()
+
+	counter += 1
 	print ("Wrong PIN. Try again.")
 	pin = int(input("Enter your PIN: "))
 
@@ -12,4 +19,4 @@ while amount > balance:
 	amount = float(input("How much money would you like to withdraw? "))
 
 balance -= amount
-print ("You have successfully withdrawn", amount, "zł. Your current balance is", round(balance, 2), "zł")
+print ("You have successfully withdrawn %.2f zł. Your current balance is %.2f zł." % (amount, balance))
